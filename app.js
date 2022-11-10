3478e862d36e9ba6504b6e1197d65cd42ace8736
Satyaki Sensarma <satyakisensarma@gmail.com>
2022-11-11 01:16:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('This is a dummy Welcome app.')
+})
+
+app.get('/api/greet/:id', (req,res) => {
+  res.send('Welcome '.concat(req.params.id))
 })
 
 //Launch listening server on port 8080
