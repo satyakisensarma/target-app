0f1b5053456fb3b5778961d40f75dd56bda88169
Satyaki Sensarma <satyakisensarma@gmail.com>
2022-11-13 21:52:42 +0530

    Added APIs

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 //Load express module with `require` directive
 var express = require('express')
 var app = express()
+const welcome = "Welcome "
+const meet = "Let's meet sometime tomorrow, "
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
   res.send('This is a dummy Welcome app.')
 })
 
-app.get('/api/greet/:id', (req,res) => {
-  res.send('Welcome '.concat(req.params.id))
+app.get('/api/greet/:id', function (req, res) {
+  res.send(welcome.concat(req.params.id))
+})
+
+app.get('/api/meet/:id', function (req, res) {
+  res.send(meet.concat(req.params.id))
 })
 
 //Launch listening server on port 8080
